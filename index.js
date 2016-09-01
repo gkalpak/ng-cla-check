0cc0e18ede550c5148ad9aa75511e9b74965a395
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-09-01 21:43:48 +0300

    refct(index): move main functionality to the `Cli` class (for better testability)

--- index.js
+++ index.js
@@ -1,85 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
-// Imports
-let minimist = require('minimist');
-
 // Imports - Local
-let Checker = require('./lib/checker');
-let config = require('./lib/config');
+let Cli = require('./lib/cli');
 
 // Run
 _main(process.argv.slice(2));
 
 // Functions - Definitions
 function _main(args) {
-  let input = getAndValidateInput(args, config);
-
-  if (input.usage) return displayUsage(config);
-
-  let checker = new Checker({
-    ghToken: input.ghToken,
-    claLabel: input.claLabel,
-    repo: input.repo
-  });
-
-  checker.
-    check(input.prNo).
-    then(onSuccess, onError);
-}
-
-function displayUsage(config) {
-  console.log(`\n ${config.usageMessage}`);
-}
-
-function getAndValidateInput(args, config) {
-  args = removeSurroundingQuotes(minimist(args));
-  if (args.usage) return {usage: true};
-
-  let ghToken = process.env[config.ghTokenVar] || null;
-  let claLabel = args.claLabel || null;
-  let repo = args.repo || null;
-  let prNo = args._[0];
-
-  if (repo && (repo.indexOf('/') === -1)) {
-    onError(`Invalid repo. Make sure to include the username (e.g. '${config.defaults.repo}').`);
-  }
-  if (!prNo) {
-    onError(`No PR specified\n\n${config.usageMessage}`);
-  }
-
-  return {ghToken, claLabel, repo, prNo};
-}
-
-function onError(err) {
-  if (err) console.error(`\n  ERROR: ${err}\n`);
-
-  reportAndExit(1);
-}
-
-function onSuccess() {
-  reportAndExit(0);
-}
-
-function removeSurroundingQuotes(args) {
-  Object.keys(args).forEach(key => {
-    let value = args[key];
-
-    if (typeof value === 'string') {
-      args[key] = value.
-        replace(/^"([^"]*)"$/, '$1').
-        replace(/^'([^']*)'$/, '$1');
-    }
-  });
-
-  return args;
-}
-
-function reportAndExit(exitCode) {
-  let message = exitCode ?
-      ':(  Unable to verify the CLA signature!' :
-      ':)  CLA signature verified successfully!';
-
-  console.log(message);
-  process.exit(exitCode || 0);
+  let cli = new Cli();
+  cli.run(args);
 }
