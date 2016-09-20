3afbf3c0c4b6f61f935d223f15768733ca0a1e5d
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-09-20 19:14:15 +0300

    refct(_run-tests-once): remove work-around for jasmine-npm v2.5.0
    The underlying issue ([jasmine-npm#88][1]) was fixed in v2.5.2 (with [a87cdad][2]).
    
    [1]: https://github.com/jasmine/jasmine-npm/issues/88
    [2]: https://github.com/jasmine/jasmine-npm/commit/a87cdadb519bc4da1c224384ec21c591f85de199

--- scripts/_run-tests-once.js
+++ scripts/_run-tests-once.js
@@ -2,7 +2,6 @@
 
 // Imports
 let Jasmine = require('jasmine');
-let util = require('util');
 
 // Constants
 const JASMINE_CONFIG = {
@@ -19,24 +18,9 @@ _main();
 // Functions - Definitions
 function _main() {
   let runner = new Jasmine();
-  let reporter = createConsoleReporter(runner);
 
   runner.loadConfig(JASMINE_CONFIG);
-  runner.addReporter(reporter);
   runner.execute();
-}
-
-function createConsoleReporter(runner) {
-  let options = {
-    jasmineCorePath: runner.jasmineCorePath,
-    print: function print() {
-      process.stdout.write(util.format.apply(this, arguments));
-    },
-    showColors: true,
-    timer: new runner.jasmine.Timer()
-  };
-
-  return new Jasmine.ConsoleReporter(options);
 }
 
 function getSpecDir() {
